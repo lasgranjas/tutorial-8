3062ca93fc440d4186d8705ad6d25267322c50e5
lasgranjas <78686390+lasgranjas@users.noreply.github.com>
2021-04-14 06:49:26 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let Jugador_principal = sprites.create(img`
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    . 5 5 1 1 1 5 5 5 5 1 1 1 5 5 . 
+    . 5 5 1 1 1 5 5 5 5 1 1 1 5 5 . 
+    . 5 5 f 1 1 5 5 5 5 f 1 1 5 5 . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 2 2 2 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    . e e e e e e e e e e e e e e . 
+    . e e e e e e e e e e e e e e . 
+    . e e e e e e e e e e e e e e . 
+    . e e e e e e . . . e e e e e . 
+    . e e e e e e . . . e e e e e . 
+    . . . 5 5 5 . . . . . 5 5 5 . . 
+    . . e e e e . . . . e e e e . . 
+    `, SpriteKind.Player)
+Jugador_principal.setPosition(80, 60)
+Jugador_principal.setVelocity(100, 100)
+controller.moveSprite(Jugador_principal)
+Jugador_principal.setStayInScreen(true)
+scene.setBackgroundColor(6)
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(Jugador_principal)
+Jugador_principal.startEffect(effects.fountain)
